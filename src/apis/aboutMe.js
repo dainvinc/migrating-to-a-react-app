3d0d78c7f5d47042e8f13b07c97566db07997ada
Vishal <vishal.koosuri1@marist.edu>
2017-12-28 21:08:07 +0000

    Created json format with my details

---
+++ src/apis/aboutMe.js
@@ -0,0 +1,28 @@
+[
+    {
+        name: "Vishal Koosuri",
+        address: "Poughkeepsie, NY-12601",
+        education: [
+            {
+                college: "Marist College",
+                startDate: "January 2016",
+                endDate: "December 2017",
+                degree: "Master's",
+                major: "Computer Science/Software Development"
+            }
+        ],
+        experience: [
+            {
+                company: "Marist College",
+                startDate: "October 2016",
+                endDate: "December 2017",
+                role: "Student Web Developer"
+            }
+        ],
+        skills: [
+            "HTML5", "CSS3", "Javascript", "Node.js", "Express.js",
+            "Angular.js", "JQuery", "AJAX", "PHP", "Java", "Python",
+            "React.js"
+        ]
+    }
+]
