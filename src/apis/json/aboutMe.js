5925a77676dc42f9a7e21403caa4b3d5fcb469bb
Vishal <vishal.koosuri1@marist.edu>
2017-12-28 22:17:45 +0000

    Updated the directory for my json object

---
+++ src/apis/json/aboutMe.js
@@ -0,0 +1,30 @@
+[    
+    {
+        name: "Vishal Koosuri",
+        headline: "Software Developer",
+        objective: "Self-motivated and passionate Software Developer with a flair for creating elegant solutions. Seeking for an employment opportunity wherein I can build, and utilize my self-taught, collegiate, and internship experiences.",
+        address: "Poughkeepsie, NY-12601",
+        education: [
+            {
+                college: "Marist College",
+                startDate: "January 2016",
+                endDate: "December 2017",
+                degree: "Master's",
+                major: "Computer Science/Software Development"
+            }
+        ],
+        experience: [
+            {
+                company: "Marist College",
+                startDate: "October 2016",
+                endDate: "December 2017",
+                role: "Student Web Developer"
+            }
+        ],
+        skills: [
+            "HTML5", "CSS3", "Javascript", "Node.js", "Express.js",
+            "Angular.js", "JQuery", "AJAX", "PHP", "Java", "Python",
+            "React.js"
+        ]
+    }
+]
